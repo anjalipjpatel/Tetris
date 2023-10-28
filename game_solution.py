aeab1a2bfcb38b644a4e96bdb251860476747368
h79771ap <anjali.patel-3@student.manchester.ac.uk>
2023-10-28 21:37:58 +0100

    Initial Plan

--- game_solution.py
+++ game_solution.py
@@ -1,1 +1,73 @@
-# initial file created
+
+# built-in imports only
+
+import tkinter as tk
+import random as ran
+
+#############################
+# map of cw workload and plan
+#############################
+
+# initial page open - new, load or leaderboard
+# enter name on same page
+
+# open leaderboard and show top x scores/search for certain username
+# be able to show  ALL scores in a list on the page
+
+####################
+# load or play game#
+####################
+# each shape an instnace of a class as it falls
+# 2d array mapping current grid
+# text file write scores etc
+
+
+# intialise points and stuff
+
+
+# cannot go outside boarder of screen
+
+
+# wasd/arrow keys to move
+
+
+# create map of shapes - get royaltry free pocs? - gonna have to be individual pieces so when the rows delete the images arent messed up
+# will the linked PIL package help with this?
+
+# 2d array which maps currently occupied spaces by shapes? - update as falls and if htis one with a val in then place there
+
+# generate shape randomly before each fall - random
+
+# check if row completed - if yes destroy and give points
+
+# check if block not in screen ie at top and hence game over
+
+# pause/quit function - pause, quit or save and quit
+
+# cheat code implement - makes all placed blocks disappear
+
+# boss key - pull up image of an email
+
+
+# if time: show upcoming blocks about to fall
+
+#############
+# save game #
+#############
+
+# store all values - scores, array, current shapes/colours into text file to load
+# return to main screen
+
+
+############
+# new game #
+############
+
+# intiialise all values to 0 and start to play game
+
+
+#############
+# load game #
+#############
+
+# find name in text file, load back values and continue after on-screen countdown
