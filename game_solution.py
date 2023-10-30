ca25d34535f552ff36420f037782998b6cb0ba51
h79771ap <anjali.patel-3@student.manchester.ac.uk>
2023-10-30 13:17:49 +0000

    functional leaderboard and homepage

--- game_solution.py
+++ game_solution.py
@@ -11,38 +11,38 @@ import time
 ################ FUNCTIONS #####################
 ################################################
 
-def AllFonts():
-    root = Tk()
-    root.title('Font Families')
-    fonts=list(font.families())
-    fonts.sort()
+# def AllFonts():
+#     root = Tk()
+#     root.title('Font Families')
+#     fonts=list(font.families())
+#     fonts.sort()
 
-    def populate(frame):
-        '''Put in the fonts'''
-        listnumber = 1
-        for item in fonts:
-            label = "listlabel" + str(listnumber)
-            label = Label(frame,text=item,font=(item, 16)).pack()
-            listnumber += 1
+#     def populate(frame):
+#         '''Put in the fonts'''
+#         listnumber = 1
+#         for item in fonts:
+#             label = "listlabel" + str(listnumber)
+#             label = Label(frame,text=item,font=(item, 16)).pack()
+#             listnumber += 1
 
-    def onFrameConfigure(canvas):
-        '''Reset the scroll region to encompass the inner frame'''
-        canvas.configure(scrollregion=canvas.bbox("all"))
+#     def onFrameConfigure(canvas):
+#         '''Reset the scroll region to encompass the inner frame'''
+#         canvas.configure(scrollregion=canvas.bbox("all"))
 
-    canvas = Canvas(root, borderwidth=0, background="#ffffff")
-    frame = Frame(canvas, background="#ffffff")
-    vsb = Scrollbar(root, orient="vertical", command=canvas.yview)
-    canvas.configure(yscrollcommand=vsb.set)
+#     canvas = Canvas(root, borderwidth=0, background="#ffffff")
+#     frame = Frame(canvas, background="#ffffff")
+#     vsb = Scrollbar(root, orient="vertical", command=canvas.yview)
+#     canvas.configure(yscrollcommand=vsb.set)
 
-    vsb.pack(side="right", fill="y")
-    canvas.pack(side="left", fill="both", expand=True)
-    canvas.create_window((4,4), window=frame, anchor="nw")
+#     vsb.pack(side="right", fill="y")
+#     canvas.pack(side="left", fill="both", expand=True)
+#     canvas.create_window((4,4), window=frame, anchor="nw")
 
-    frame.bind("<Configure>", lambda event, canvas=canvas: onFrameConfigure(canvas))
+#     frame.bind("<Configure>", lambda event, canvas=canvas: onFrameConfigure(canvas))
 
-    populate(frame)
+#     populate(frame)
 
-    root.mainloop()
+#     root.mainloop()
 #Fonts()
 
 #############
@@ -121,7 +121,8 @@ def Sort(arr):
 
 
 
-##########################
+##########################################################################
+
 def NewGameClicked():
     GetUsername()
     WipeAllWidgets()
@@ -134,13 +135,12 @@ def LoadGameClicked():
 # be able to show  ALL scores in a list on the page
 def LeaderboardClicked():
     #######################################
-    # add vertical scroll bar functionality
+    # add vertical scroll bar functionality if time
     #######################################
-    GetUsername()
     WipeAllWidgets()
     ShowLeaderboard()
 
-##########################
+##########################################################################
 
 #############################
 # map of cw workload and plan
@@ -150,7 +150,6 @@ root = tk.Tk()
 # initial page open - new, load or leaderboard
 # enter name on same page
 HomeWindow()
-
 
 
 ####################
